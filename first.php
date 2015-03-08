6e7b486484959330515a5ee8dbfd8e34e1467203
Melissa Malpica <Malpicadz@gmail.com>
2015-03-08 12:03:27 -0400

    Learning and practicing PHP and refreshing my git command knowledge. This is my first simple test and I am executing my PHP script via mamp server so that I can view output.

---
+++ first.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP, MySQL and Apache All in One - Practice</title>
+</head>
+
+<body>
+
+<?php
+	echo "<h1>Welcome to my first script!</h1>";
+	?>
+
+	<p>OR</p>
+
+	<h3><?php print "Hello Again"; ?></h3>
+
+</body>
+</html>
