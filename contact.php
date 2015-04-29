d30bed1b4f09012208c16679aa66957693e07e9d
Melissa Malpica <Malpicadz@gmail.com>
2015-04-29 15:52:58 -0400

    Adding updates from working of MAMP to post to my GH Repo

--- contact.php
+++ contact.php
@@ -1,7 +1,15 @@
-<?php include('inc/header.php'); ?>
+<?php 
+
+$pageTitle = "Contact Mike";
+
+$section = "contact";
+
+include('inc/header.php'); ?>
 
 	<div class="section page">
 		
 		<h1>Contact</h1>
 		
 	</div>
+
+	<?php include('inc/footer.php'); ?>
--- index.php
+++ index.php
@@ -1,29 +1,8 @@
-<html>
-<head>
-	<title><?php echo "Shirts 4 Mike"; ?></title>
-	<link rel="stylesheet" href="css/style.css" type="text/css">
-	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Oswald:400,700" type="text/css">
-	<link rel="shortcut icon" href="favicon.ico">
-</head>
-<body>
+<?php 
 
-	<div class="header">
+$pageTitle = "Unique Tee Shirt Designs by Mike the Frog";
 
-		<div class="wrapper">
-
-			<h1 class="branding-title"><a href="./">Shirts 4 Mike</a></h1>
-
-			<ul class="nav">
-				<li class="shirts"><a href="#">Shirts</a></li>
-				<li class="contact"><a href="#">Contact</a></li>
-				<li class="cart"><a href="#">Shopping Cart</a></li>
-			</ul>
-
-		</div>
-
-	</div>
-
-	<div id="content">
+	include('inc/header.php'); ?>
 
 		<div class="section banner">
 
@@ -75,20 +54,7 @@
 
 	</div>
 
-	<div class="footer">
-
-		<div class="wrapper">
-
-			<ul>		
-				<li><a href="http://twitter.com/treehouse">Twitter</a></li>
-				<li><a href="https://www.facebook.com/TeamTreehouse">Facebook</a></li>
-			</ul>
-
-			<p>&copy;<?php echo date('Y'); ?> Shirts 4 Mike</p>
-
-		</div>
-	
-	</div>
+	<?php include('inc/footer.php'); ?>
 
 </body>
 </html>
